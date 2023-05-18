389fc722fc1118535923557f261a754fe3670826
Adam Oehmen <oehmena@wit.edu>
2023-05-18 10:31:08 -0400

    Added code to take in planetext

--- Enigma_and_Bombe/Driver.cpp
+++ Enigma_and_Bombe/Driver.cpp
@@ -2,7 +2,14 @@
 #include <string>
 #include "Class1.h"
 
+using namespace std;
+string planetext;
+
 int main() //This is the main
 {
+	cout << "What is the message you want to send\n"; //get the massage we want to encode
+	getline(cin,planetext);
+
+	return 0;
 
 }
