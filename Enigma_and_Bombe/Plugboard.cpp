f61e2d4e5bf8094112eed9a9d61051be5e2f8fed
Adam Oehmen <oehmena@wit.edu>
2023-05-25 09:03:00 -0400

    Added switch to plugboard and put test in main.

--- Enigma_and_Bombe/Plugboard.cpp
+++ Enigma_and_Bombe/Plugboard.cpp
@@ -3,7 +3,7 @@ Plugboard::Plugboard()
 {
 	for (int i = 0; i < 24; i++)
 	{
-		connections[i] = i + 1;
+		connections[i] = i;
 	}
 }
 
@@ -15,19 +15,19 @@ string Plugboard::encodeMessage(string input_message)
 
 void Plugboard::setPlugPos(int PlugPos[])
 {
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 10; i+=2)
 	{
 		int tempi = PlugPos[i];
-		int temp = connections[tempi];
+		int temp = PlugPos[i+1];
 		connections[tempi] = PlugPos[i + 1];
 		connections[temp] = PlugPos[i];
 	}
 }
 
 
-void Plugboard::getPlugPos()
+int Plugboard::PlugSwitch(int letter)
 {
-
+	return(connections[letter]);
 }
 
 Plugboard::~Plugboard()
