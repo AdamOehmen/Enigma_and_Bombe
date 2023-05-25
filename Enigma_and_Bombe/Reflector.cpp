463ec12163f8b546b532abd2ae9fa0c403056a14
EthanM827 <ethanmcfetridge@gmail.com>
2023-05-25 13:49:32 -0400

    Commented Reflector class

--- Enigma_and_Bombe/Reflector.cpp
+++ Enigma_and_Bombe/Reflector.cpp
@@ -1,25 +1,33 @@
 #include "Reflector.h"
 
+// Default constructor, uses hardcoded reflector vals
 Reflector::Reflector() {
 
 }
 
 Reflector::Reflector(int reflectorNum) {
+	// TODO: Add multiple reflectors, change pairings[13][2] to match new reflector in this constructor
 
 }
 
 int Reflector::reflect(int input) {
 	try {
 		for (int i = 0; i <= 12; i++) {
+			// Check left side of the pairings array for input
 			if (pairings[i][0] == input) {
+				// Return paired int if found
 				return pairings[i][1];
 			}
+			// Check right side of the pairings array
 			else if (pairings[i][1] == input) {
+				// Return paired int if found
 				return pairings[i][0];
 			}
 		}
+		// Throw error if input is not in pairings array
 		throw(input);
 	}
+	// Print error msg
 	catch (int wrongInput) {
 		cout << "Error in Reflector::reflect: Input (" << wrongInput << ") not listed in reflector.\n";
 	}
