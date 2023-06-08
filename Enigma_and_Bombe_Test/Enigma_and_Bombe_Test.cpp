b3844374c09ee86531058bdf31aa5a9dd4dc8279
Derek Huang <huangd8@wit.edu>
2023-06-08 08:29:25 -0400

    Example Test Case

--- Enigma_and_Bombe_Test/Enigma_and_Bombe_Test.cpp
+++ Enigma_and_Bombe_Test/Enigma_and_Bombe_Test.cpp
@@ -3,14 +3,30 @@
 #include "../Enigma_and_Bombe/Driver.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-
+/*
+* Example Test Method:
+	TEST_METHOD(TestClassInit)
+	{
+		std::string name = "Bill";
+		MyClass mc(name);
+		Assert::AreEqual(name, mc.GetName());
+	}
+*/
 namespace EnigmaandBombeTest
 {
 	TEST_CLASS(EnigmaandBombeTest)
 	{
 	public:
-		
+		// Unit Tests for Rotors
 		TEST_METHOD(Testmethod1)
+		{
+		}
+		// Unit Tests for Reflector
+		TEST_METHOD(Testmethod2)
+		{
+		}
+		// Unit Tests for Plugboard
+		TEST_METHOD(Testmethod3)
 		{
 		}
 	};
