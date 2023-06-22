3d9bd93a2665fe937419166249ee31747921067d
EthanM827 <ethanmcfetridge@gmail.com>
2023-06-22 08:45:11 -0400

    commented and tested full enigma machine

--- Enigma_and_Bombe/Driver.cpp
+++ Enigma_and_Bombe/Driver.cpp
@@ -6,56 +6,73 @@ string plaintext;
 int main() //This is the main
 {
 	
-	cout << "What is the message you want to send\n"; //get the massage we want to encode
+	cout << "What is the message you want to send\n"; //get the message we want to encode
 	getline(cin,plaintext);
 	plaintext = delSpaces(plaintext);  // comment
 	int messageSize = plaintext.size();
 	vector<int> plainNum(messageSize);
 
 	
+	// Convert input message to array of numbers
 	for (int i = 0; i < messageSize; i++)
 	{
 		plainNum[i] = letterToNum(plaintext[i]);
 	}
 
-	
+
+	// Create plugboard with 5 hard-coded pairs of numbers
 	int arry[10] = { 2, 7, 10, 5, 16, 20, 22, 12, 6, 23};
 	Plugboard plug{};
 	plug.setPlugPos(arry);
 	Reflector reflector;
 
 	int numRotors = 3;
-	Rotor rotors[3] = { Rotor(), Rotor(), Rotor() };
+	// Create a vector with all of the rotors so we can iterate through it
+	// Each has hard-coded values for now
+	vector<Rotor> rotors;
+	for (int i = 0; i < numRotors; i++) {
+		rotors.push_back(Rotor());
+	}
 	for (int i = 0; i < messageSize; i++) {
+		// Send input through plugboard
 		plainNum[i] = plug.PlugSwitch(plainNum[i]);
 		cout << "After plugboard: " << plainNum[i] << endl;;
 
+		// Send plugboard output through each rotor in sequence
 		for (int j = 0; j < numRotors; j++) {
 			plainNum[i] = rotors[j].getScramblePos(plainNum[i]);
 			cout << "After rotor " << j+1 << ": " << plainNum[i] << endl;
 		}
-
+		
+		// Send output of rotors through reflector
 		plainNum[i] = reflector.reflect(plainNum[i]);
 		cout << "After reflector: " << plainNum[i] << endl;
 
+		// Send reflector back through each rotor, in reverse sequential order
 		for (int j = numRotors - 1; j >= 0; j--) {
 			plainNum[i] = rotors[j].getScramblePos(plainNum[i]);
 			cout << "After rotor " << j + 1 << ": " << plainNum[i] << endl;
 		}
 
-		cout << "ROTATE ROTOR 1" << endl;
+		// Rotate rotor #1 after every encrypted input
 		rotors[0].rotate();
+		cout << "ROTATE ROTOR 1" << endl;
+
+		
 		bool lastRotated = true;
 		for (int k = 1; k < numRotors; k++) {
+			// Rotate the next rotor IF the notch of the previous matches the current position, AND the previous just rotated
 			if ((rotors[k - 1].getNotch() == rotors[k - 1].getPos()) && lastRotated) {
 				rotors[k].rotate();
 				cout << "ROTATE ROTOR " << (k + 1) << endl;
 				lastRotated = true;
 			}
+			// Otherwise, update value of lastRotated
 			else {
 				lastRotated = false;
 			}
 		}
+		// Print final letter to screen
 		cout << numToLetter(plainNum[i]) << endl;
 	}
 
