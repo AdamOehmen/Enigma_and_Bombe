6030326e599fc20e1948f4bf1aa73a8e6a5024a8
Derek Wong <99dwong@gmail.com>
2023-06-29 13:37:33 -0400

    Login, get last message order, revised db_store function

--- Enigma_and_Bombe/Driver.cpp
+++ Enigma_and_Bombe/Driver.cpp
@@ -2,7 +2,9 @@
 #include "sqlite3.h"
 
 string plaintext;
-int msg_order = 1;
+sqlite3* db;
+
+int msg_order;
 int plug_name = 1;
 int rotor_name = 1;
 int rotor_usr, plug_usr;
@@ -20,11 +22,87 @@ static int callback(void* data, int argc, char** argv, char** azColName)
 
 	return 0;
 }
-/*
-Have something to pull from data to see which message order we currently are on
-*/
+
+static int login(string usr, string pwd) {
+	int exit = sqlite3_open("enigma_bombe.db", &db); // open database
+	if (exit != SQLITE_OK) {	// check if database is opened
+		cout << "error" << endl;
+	}
+	else {
+		cout << "open success" << endl;
+	}
+	string query = "SELECT 1 FROM Login WHERE Username = '" + usr + "';";	// SQL statement selecting 1 and inserting into result set if username is found else select 0
+	sqlite3_stmt* stmt;
+	int result;
+	int rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr); // Prepare the statement
+	if (rc != SQLITE_OK) {
+		cout << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
+		return 0;
+	}
+	rc = sqlite3_step(stmt); // Execute the statement
+	if (rc != SQLITE_ROW) {
+		cout << "Incorrect Username or Password" << endl;
+		return 0;
+	}
+
+	int count = sqlite3_column_int(stmt, 0); // Get the count from the result set
+	if (count > 0) {
+		sqlite3_finalize(stmt); // Finalize the statement
+	}
+	else {
+		cout << "Incorrect Username or Password" << endl;
+		return 0;
+	}
+
+	query = ("SELECT 1 FROM Login WHERE Password = '" + pwd + "';");	// SQL statement selecting 1 and inserting into result set if password is found else select 0
+
+	rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr); // Prepare the statement
+	if (rc != SQLITE_OK) {
+		cout << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
+		return 0;
+	}
+	rc = sqlite3_step(stmt); // Execute the statement
+	if (rc != SQLITE_ROW) {
+		cout << "Incorrect Username or Password" << endl;
+		return 0;
+	}
+
+	count = sqlite3_column_int(stmt, 0); // Get the count from the result set
+	if (count > 0) {
+		cout << "Welcome to Enigma!" << endl;
+		sqlite3_finalize(stmt); // Finalize the statement
+		sqlite3_close(db);	// close database 
+		return 1;
+	}
+	else {
+		cout << "Incorrect Username or Password" << endl;
+		return 0;
+	}
+
+}
+static void last_msg() {
+	string query;
+	sqlite3_stmt* stmt;
+	int exit = 0;
+	query = "SELECT MAX(messageOrder) FROM Past_Messages;";
+	int rc = sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr); // Prepare the statement
+	if (rc != SQLITE_OK) {
+		cout << "Failed to prepare statement: " << sqlite3_errmsg(db) << endl;
+	}
+	rc = sqlite3_step(stmt); // Execute the statement
+	if (rc != SQLITE_ROW) {
+		cout << "Statement failed" << endl;
+	}
+	int count = sqlite3_column_int(stmt, 0); // Get the maximum message order from result set
+	if (count >= 0) {
+		sqlite3_finalize(stmt); // Finalize the statement
+		msg_order = count + 1;	// set msg_order as max message order + 1
+	}
+	else {
+		cout << "No results!" << endl;
+	}
+}
 static void db_store(string tbl) {
-	sqlite3* db;
 	int exit = 0;
 	string query;
 	exit = sqlite3_open("enigma_bombe.db", &db); // open database
@@ -36,32 +114,45 @@ static void db_store(string tbl) {
 	}
 	if (tbl == "Past_Messages") {
 		int rotor_used, plug_used;
-		query = "INSERT INTO " + tbl + " VALUES(" + to_string(msg_order) + ", '" + plaintext + "', 'placeholder', " + to_string(rotor_usr) + ", " + to_string(plug_usr)+ "); ";	// construct query using user input, will replace placeholder with actual encrypted msg
-		msg_order++; // update msg order 
+		last_msg();	// grab the last message order
+		query = "INSERT INTO " + tbl + " VALUES(" + to_string(msg_order) + ", '" + plaintext + "', 'placeholder', " 
+		+ to_string(rotor_usr) + ", " + to_string(plug_usr)+ "); ";	// construct query using user input, will replace placeholder with actual encrypted msg
 	}
 	else if (tbl == "Plugboard_Settings") {
 		query = "INSERT INTO " + tbl + " VALUES(" + to_string(plug_name) + ", 'ABCD'); ";	// construct query using user input, will replace placeholder with actual setting
 		plug_name++; // update plug name
 	}
 	else if (tbl == "Rotor_Settings") {
-		query = "INSERT INTO " + tbl + " VALUES(" + to_string(rotor_name) + ", 1); ";	// construct query using user input, will replace placeholder with actual setting and notch
+		query = "INSERT INTO " + tbl + " VALUES(" + to_string(rotor_name) + ", 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1); ";	// construct query using user input, will replace placeholder with actual setting and notch
 		rotor_name++; // update rotor name 
 	}
 	cout << query << endl;
 	exit = sqlite3_exec(db, query.c_str(), callback, NULL, NULL);	// execute the query command 
 	if (exit != SQLITE_OK)	// check if query has been executed successfully 
 	{
-		cerr << "Error Insert" << endl;
+		cerr << "Error Insert: " << sqlite3_errmsg(db)<<endl;
 	}
-	else
+	else {
 		cout << "Inserted into Database Successfully!" << endl;
-	sqlite3_close(db);	// close database after writing 
+		sqlite3_close(db);	// close database after writing 
+	}
 }
+
 int main() //This is the main
 {
 	sqlite3_stmt* st;
 	string sql3;
-
+	string usr_username;
+	string usr_password;
+	// Login into Enigma
+	cout << "-------------Please login to Enigma-------------" << endl;
+	cout << "Username: ";	cin >> usr_username;
+	cout << "Password: ";	cin >> usr_password;
+	cin.ignore();	// clear queue
+	if (login(usr_username, usr_password) == 0) {
+		return 0;
+	}
+	
 	cout << "What is the message you want to send\n"; //get the message we want to encode
 	getline(cin,plaintext);  // get msg
 	plaintext = delSpaces(plaintext);  // delete spaces of msg
