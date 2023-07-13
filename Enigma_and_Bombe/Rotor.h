fafde4a33a0a3730e1e6bcbcc765731c87d9fc86
Adam Oehmen <oehmena@wit.edu>
2023-07-13 08:34:49 -0400

    rt

--- Enigma_and_Bombe/Rotor.h
+++ Enigma_and_Bombe/Rotor.h
@@ -8,8 +8,6 @@ private:
 	int notch; // The notch is a signle position that will tell the next rotor in line to rotate
 	int scramble[26] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 							 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
-		//= { 20, 13, 21, 18, 23, 1, 12, 14, 7, 10, 11, 25, 19,
-		//					 4, 9, 2, 0, 17, 16, 15, 6, 22, 5, 4, 3, 8 }; // The order of the scrambled numbers on the rotor
 public:
 	Rotor();
 	int getPosition();
