2e45e9504aae67c8a01fb0364d82647f17bcff9e
Derek Huang <huangd8@wit.edu>
2023-07-18 11:50:17 -0400

    Added Settings Menu, Full Plugboard DB Integration, and last_msg() changes

--- Enigma_and_Bombe/Plugboard.h
+++ Enigma_and_Bombe/Plugboard.h
@@ -10,6 +10,8 @@ class Plugboard
 public:
 	Plugboard();
 	void createPlugboard();
+	string returnPlugLet();
+	void DB_Extract(string db_setting);
 	int test_createPlugboard(int test_pos[10][2], char test_let[10][2]);
 	void setPlugPos();
 	int test_setPlugPos(int test_swap[26]);
