ac5cf06cc32cc4987e171c0caa93ef791b3a7ae6
Derek Wong <99dwong@gmail.com>
2023-07-20 22:05:02 -0400

    Final Enigma Merge w/ Rotors

--- Enigma_and_Bombe/Rotor.cpp
+++ Enigma_and_Bombe/Rotor.cpp
@@ -1,37 +1,165 @@
 #include <vector>
 #include <iostream>
-
+#include <sstream>
 #include "Rotor.h"
 int lettersToNum(char letter);
 char Alp[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+string query;
+sqlite3* dab;
+int YN = 0;
+string str_scramble = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+							 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
 // Default Constructor, sets start position and notch to '0' 
 // and reference rotor to NULL if no values are given
+static int callback(void* data, int argc, char** argv, char** azColName)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+
+	return 0;
+}
+
+string pull_rotor_set(int rotor) {
+	int exit = sqlite3_open("enigma_bombe.db", &dab); // open database
+	if (exit != SQLITE_OK) {	// check if database is opened
+		cout << "error" << endl;
+	}
+	else {
+		cout << "open success" << endl;
+	}
+	string query = "SELECT rotorSetting FROM Rotor_Settings WHERE rotorName = " + to_string(rotor) + ";";	// SQL statement selecting rotor setting
+	sqlite3_stmt* stmt;
+	int rc = sqlite3_prepare_v2(dab, query.c_str(), -1, &stmt, nullptr); // Prepare the statement
+	if (rc != SQLITE_OK) {
+		cout << "Failed to prepare statement: " << sqlite3_errmsg(dab) << endl;	// Check if statement is prepared correctly
+	}
+	rc = sqlite3_step(stmt); // Execute the statement
+	if (rc != SQLITE_ROW) {
+		cout << "Rotor Doesn't exist" << endl;	// Check if plugboard exists
+	}
+
+	size_t length = strlen(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));  // calculate the length using reinterpret_cast since strlen expects a string
+	int size = length * sizeof(unsigned char);  // calculate the total size
+
+	string result = "";
+	for (int i = 0; i < size; i++) {
+		char temp = sqlite3_column_text(stmt, 0)[i];	// temp char set to each index of char array
+		result = result + temp;	// append temp to resulting string
+	}
+	sqlite3_finalize(stmt); // Finalize the statement
+	sqlite3_close(dab);	// close dab
+	return result;
+}
+int pull_rotor_notch(int rotor) {
+	int exit = sqlite3_open("enigma_bombe.db", &dab); // open database
+	if (exit != SQLITE_OK) {	// check if database is opened
+		cout << "error" << endl;
+	}
+	else {
+		cout << "open success" << endl;
+	}
+	string query = "SELECT notch FROM Rotor_Settings WHERE rotorName = " + to_string(rotor) + ";";	// SQL statement selecting rotor setting
+	sqlite3_stmt* stmt;
+	int rc = sqlite3_prepare_v2(dab, query.c_str(), -1, &stmt, nullptr); // Prepare the statement
+	if (rc != SQLITE_OK) {
+		cout << "Failed to prepare statement: " << sqlite3_errmsg(dab) << endl;	// Check if statement is prepared correctly
+	}
+	rc = sqlite3_step(stmt); // Execute the statement
+	if (rc != SQLITE_ROW) {
+		cout << "Rotor Doesn't exist" << endl;	// Check if plugboard exists
+	}
+
+	int result = sqlite3_column_int(stmt, 0);	// save notch value into result
+	sqlite3_finalize(stmt); // Finalize the statement
+	sqlite3_close(dab);	// close dab
+	return result;
+}
+
 Rotor::Rotor()
 {
-	cout << "These are your rotors, would you like to create a new one?";
-	string CLearWhenAddingDataBase;
-	cin >> CLearWhenAddingDataBase;
-	cin.ignore();
-	this->notch = 0;
-
-}
-
-Rotor::Rotor(int newScramble[], int newNotch) {
-	for (int i = 0; i <= 25; i++) {
-		this->scramble[i] = newScramble[i];
+	int exit = 0;
+	char temp;
+	sqlite3_stmt* stmt;
+	query = "SELECT * FROM Rotor_Settings;";
+	int pullm = sqlite3_open("enigma_bombe.db", &dab); // open database
+	if (pullm != SQLITE_OK) {	// check if database is opened
+		cout << "error" << endl;
 	}
-	this->notch = newNotch;
+	else
+	{
+		sqlite3_exec(dab, query.c_str(), callback, NULL, NULL);
+	}
+
+
+	while (1) {
+		cout << "These are the current rotors" << endl;
+		cout << "Would you like to create a new one? (Y/N): ";
+		string ClearWhenAddingDataBase;
+		cin >> ClearWhenAddingDataBase;
+		cin.ignore();
+
+		if (ClearWhenAddingDataBase == "N")
+		{
+			cout << "Which rotors would you like to use: ";
+			cin >> this->rtrName;
+			this->scramble = pull_rotor_set(this->rtrName);
+			cout << "Setting is " << this->scramble << endl;
+			for (int i = 0; i < scramble.length(); i++)
+			{
+				this->scramble[i] = lettersToNum(this->scramble[i]);
+			}
+			this->notch = pull_rotor_notch(this->rtrName);
+			YN = 0;
+			break;
+		}
+		else if (ClearWhenAddingDataBase == "Y")
+		{
+			for (int i = 0; i < 26; i++)
+			{
+				cout << Alp[i] << " goes to what letter: ";
+				cin >> temp;
+				str_scramble[i] = temp;
+				this->scramble[i] = lettersToNum(temp);
+			}
+			cout << "What letter has the notch: ";
+			cin >> temp;
+			this->notch = lettersToNum(temp);
+			YN = 1;
+			break;
+		}
+		else
+		{
+			cout << "Please Enter Y or N" << endl;
+		}
+
+	}
+
 }
 
-int Rotor::getPosition()
+int Rotor::getName()
 {
-	return this->position;
+	return this->rtrName;
 }
 
 // Returns notch position
-int Rotor::getNotch() 
+int Rotor::getNotch()
 {
 	return this->notch;
+}
+
+string Rotor::getStrScramble() {
+	return str_scramble;
+}
+string Rotor::getScramble()
+{
+	return this->scramble;
 }
 
 // Simulates a rotation of the Rotor by incrementing its position 
@@ -117,7 +245,7 @@ void Rotor::UI_Scramble()
 	for (int i = 0; i < 26; i++)
 	{
 		char ctemp;
-		int itemp, ytemp;
+		int itemp;
 		string sttemp;
 		cout << Alp[i] << " is link to: ";
 		getline(cin, sttemp);
@@ -137,10 +265,14 @@ void Rotor::UI_Scramble()
 				scramble[i] = itemp;
 			}
 		}
-		//Add stuff for db
+		//Add stuff for dab
 	}
 }
 
+int Rotor::getYN()
+{
+	return YN;
+}
 
 
 Rotor::~Rotor()
